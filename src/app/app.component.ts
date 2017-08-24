b19ef58675dd5ff8457035db65be5b8fa967f9e4
theTrueMikeBrown <theTrueMikeBrown@gmail.com>
2017-08-24 09:31:41 -0600

    some work on logging in and logging out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { FirebaseUISignInSuccess } from 'firebaseui-angular';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    displayName: string;
+    uid: string;
+    email: string;
+    photoURL: string;
+    loggedIn: boolean;
+
+    constructor(private afAuth: AngularFireAuth) {
+        this.loggedIn = false;
+    }
+
+    signIn(data: User) {
+        if (data != null) {
+            this.displayName = data.displayName;
+            this.uid = data.uid;
+            this.email = data.email;
+            this.photoURL = data.photoURL;
+            this.loggedIn = true;
+        }
+    }
+
+    ngOnInit(): void {
+        this.afAuth.authState.subscribe(d => this.signIn(d));
+    }
+
+    logout() {
+        this.afAuth.auth.signOut();
+    }
+
+    successCallback(data: FirebaseUISignInSuccess) {
+        console.log(data);
+        this.signIn(data.currentUser);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AuthMethods, AuthProvider, FirebaseUIAuthConfig, FirebaseUIModule, AuthProviderWithCustomConfig } from 'firebaseui-angular';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+const firebaseUiAuthConfig: FirebaseUIAuthConfig = {
+    providers: [
+        AuthProvider.Google,
+        AuthProvider.Password
+    ],
+    method: AuthMethods.Popup,
+    tos: 'tos.html'
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireAuthModule,
+        FirebaseUIModule.forRoot(firebaseUiAuthConfig)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
