ac85c61949045f8ad498716c5eecd6217b893d4e
Mike Brown <mbrown@1800contacts.com>
2017-08-29 15:00:11 -0600

    foo

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,4 +1,4 @@
-﻿import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 import { Game } from './game';
