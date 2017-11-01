4df69337ef5a42fdcc154aa7a272d2079f3e3d4d
theTrueMikeBrown <theTrueMikeBrown@gmail.com>
2017-11-01 10:47:16 -0600

    making it deployable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
-export const environment = {
-  production: true
+﻿export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCx5XO1UAgRGj3eDoHBgLSyoXKdvzX28Eg",
+        authDomain: "playtesthub.firebaseapp.com",
+        databaseURL: "https://playtesthub.firebaseio.com",
+        projectId: "playtesthub",
+        storageBucket: "playtesthub.appspot.com",
+        messagingSenderId: "10104267430"
+    }
 };
