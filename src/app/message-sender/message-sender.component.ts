1108a614823f2ba06c82252e5fa6fdf7404eb9ac
Mike Brown <mbrown@1800contacts.com>
2018-02-16 14:14:56 -0700

    final check in on the other computer

--- src/app/message-sender/message-sender.component.ts
+++ src/app/message-sender/message-sender.component.ts
@@ -1,8 +1,8 @@
-﻿import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
-import { startWith } from 'rxjs/add/operators/startWith';
-import { map } from 'rxjs/add/operator/map';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
 
 @Component({
     selector: 'app-message-sender',
@@ -23,17 +23,16 @@ export class MessageSenderComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-        this.filteredOptions = this.myControl.valueChanges
-            .pipe(
-            startWith(''),
-            map(val => this.filter(val))
-            );
+        this.filteredOptions = this.myControl.valueChanges.
+            .map((value, index) => { return value; });
+            //.pipe(
+            //startWith(''),
+            //map(val => this.filter(val))
+            //);
     }
 
     filter(val: string): string[] {
         return this.options.filter(option =>
             option.toLowerCase().indexOf(val.toLowerCase()) === 0);
     }
-
-
 }
